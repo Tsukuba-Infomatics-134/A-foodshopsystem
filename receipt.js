543fe5342ef04f744e4e3d95386573116eb2b04c
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2025-09-03 10:26:41 +0900

    印刷内容の調整

--- receipt.js
+++ receipt.js
@@ -39,31 +39,62 @@ function receipt_checkPrinterCondition() {
 }
 
 
+function _getYenText(price, withSpace=true) {
+    // 3桁・4桁円しか想定されないため、それに合わせた金額表示を行う。3桁円の場合は空白2個が先頭に含まれる
+    let price_str = withSpace ? `  \\${price}` : `\\${price}`;
+    if (price >= 1000) {
+        price_str = `\\${Math.floor(price/1000)},${price%1000}`;  // 1000円以上のときはカンマを付ける
+    }
+    return price_str
+}
+
+
+function _getReceiptStringLength(text) {
+    let ret = 0;
+    for(let i in text) {
+        // 全角半角判定の正規表現
+        if (i.match(/^[^\x01-\x7E\xA1-\xDF]+$/)) {
+            ret += 2
+        } else {
+            ret += 1
+        }
+    }
+    return ret;
+}
+
+
+function _getMobileURL(order_id, password) {
+    return `https://api.yaakiyu.f5.si/M/${order_id}/${password}`;
+}
+
+
 function _makeReceiptProductsLine(builder, class_number, item_id, count) {
     const item_index = item_id == "item_1" ? 0 : 1;
     const titleData = [
-        "ベビーカステラ(プレーン)   \n",
-        "ベビーカステラ(抹茶)       \n",
-        "お好み焼き(豚玉)           \n",
-        "お好み焼き(豚玉チーズ)     \n",
-        "みたらしきなこ団子         \n",
-        "カラフルあんこ団子         \n",
-        "かき氷(抹茶白玉小豆)       \n",
-        "かき氷(マンゴー)           \n",
-        "ポテト(コンソメ)           \n",
-        "ポテト(バター醤油)         \n",
-        "たこ焼き(ソース)           \n",
-        "たこ焼き(明太もち)         \n",
+        "ベビーカステラ(プレーン)  ",
+        "ベビーカステラ(抹茶)  ",
+        "お好み焼き(豚玉)  ",
+        "お好み焼き(豚玉チーズ)  ",
+        "みたらしきなこ団子  ",
+        "カラフルあんこ団子  ",
+        "かき氷(抹茶白玉小豆)  ",
+        "かき氷(マンゴー)  ",
+        "ポテト(コンソメ)  ",
+        "ポテト(バター醤油)  ",
+        "たこ焼き(ソース)  ",
+        "たこ焼き(明太もち)  ",
     ];
     let ret = builder.createAlignmentElement({position:'left'});
-    ret += builder.createTextElement({data:titleData[(class_number - 1) * 2 + item_index]});
+    const itemname = titleData[(class_number - 1) * 2 + item_index];
+    ret += builder.createTextElement({data:itemname});
     const price = priceData[class_number-1][item_index];
-    let space = "";
-    if (price * count < 1000) {
-        space = " ";  // 1000円未満のときは空白の量を調整する
+    let price_str = `${count}個  ${_getYenText(price*count)}`
+    if (_getReceiptStringLength(itemname + price_str) > 32) {
+        // 2行に分ける
+        ret += builder.createTextElement({data:"\n"})
     }
     ret += builder.createAlignmentElement({position:'right'});
-    ret += builder.createTextElement({data:`${count}個       ${space}\\${price*count}\n`});
+    ret += builder.createTextElement({data:price_str + '\n'});
     return [ret, price*count]
 }
 
@@ -76,7 +107,7 @@ function _onSendMessageApi(data) {
     const builder = new StarWebPrintBuilder();
 
     let request = '';
-    let total_money = 0;
+    let total_price = 0;
 
     request += builder.createInitializationElement();
 
@@ -85,37 +116,35 @@ function _onSendMessageApi(data) {
     request += builder.createAlignmentElement({position:'right'});
     request += builder.createLogoElement({number:1});
     request += builder.createTextElement({data:'TOIN FESTIVAL 2025\n'});
+    request += builder.createTextElement({data:'\n'});
     request += builder.createAlignmentElement({position:'left'});
-
+    request += builder.createTextElement({data:"ご利用ありがとうございます。\n"});
     request += builder.createTextElement({data:'\n'});
 
-    request += builder.createAlignmentElement({position:'center'});
-    request += builder.createTextElement({data:"またのご利用をお待ちしております。\n"});
-
-    request += builder.createTextElement({data:'\n'});
-
-    // request += builder.createTextElement({data:`Apple                $20.00\n`});
     const item_1_data = _makeReceiptProductsLine(builder, data["class_number"], "item_1", data["item_1"]);
     request += item_1_data[0];
-    total_money += item_1_data[1];
+    total_price += item_1_data[1];
     const item_2_data = _makeReceiptProductsLine(builder, data["class_number"], "item_2", data["item_2"]);
     request += item_2_data[0];
-    total_money += item_2_data[1];
-    request += builder.createTextElement({underline:true, data:'\n'});
+    total_price += item_2_data[1];
+    request += builder.createAlignmentElement({position:'left'});
+    request += builder.createTextElement({underline:true, data:'                                \n'});
     request += builder.createTextElement({underline:false});
-    request += builder.createTextElement({emphasis:true});
-    request += builder.createTextElement({width:2, data:'合計'});
-    let space = "";
-    if (total_money < 1000) {
-        space = " ";
-    }
-    request += builder.createTextElement({width:1, data:'  ' + space});
-    request += builder.createTextElement({width:2, data:`\\${total_money}\n`});
-    request += builder.createTextElement({width:1});
-    request += builder.createTextElement({emphasis:false});
 
-    request += builder.createTextElement({data:'\n'});
-    request += builder.createTextElement({data:'\n'});
+    request += builder.createTextElement({width:2, data:'合計'});
+    request += builder.createAlignmentElement({position:'right'});
+    request += builder.createTextElement({width:2, data:`${_getYenText(total_price)}\n`});
+    request += builder.createTextElement({width:1});
+
+    request += builder.createAlignmentElement({position:'left'});
+    request += builder.createTextElement({underline:true, data:'                                \n'});
+    request += builder.createTextElement({underline:false});
+
+    request += builder.createAlignmentElement({position:'center'});
+    request += builder.createTextElement({data:"整理番号\n"});
+    request += builder.createTextElement({width:2, emphasis:true, data: data["order_id"]})
+
+    request += builder.createTextElement({data:'発行：2025/09/03 10:05\n'});
 
     request += builder.createTextElement({characterspace:0});
     request += builder.createFeedElement({line:1});
